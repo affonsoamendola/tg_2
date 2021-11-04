8b1c082679357e1b9cb52fd9071e1f34bdc6bcb8
Affonso Amendola <affonso.gino.neto@usp.br>
2021-11-04 12:45:22 -0300

    Finished the file downloading system

--- flask/beacondb/app/home/views.py
+++ flask/beacondb/app/home/views.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, url_for, current_app
+from flask import flash, redirect, render_template, url_for, current_app, request, send_file, safe_join
 from werkzeug.utils import secure_filename
 
 from .forms import UploadForm
@@ -8,14 +8,24 @@ from ..models import SpectraEntry
 
 from .. import db
 
+from astropy.io import fits
+
 from os.path import join, dirname, realpath
+
+from sys import stderr
 
 @home.route('/')
 def homepage():
 	return render_template("home/index.html", title="BeaconDB")
 
-@home.route('/dashboard')
+@home.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
+	if(request.method == 'POST'):
+		try:
+			return send_file(safe_join(dirname(realpath(__file__)), "file_storage", request.form["download_button"]))
+		except	FileNotFoundError:
+			abort(404)
+
 	recent_additions = SpectraEntry.query.all()
 
 	return render_template("home/dashboard.html", recent_additions=recent_additions, title="BeaconDB - Dashboard")
@@ -27,10 +37,30 @@ def upload():
 	if(form.validate_on_submit()):
 		filename = secure_filename(form.file.data.filename)
 
-		form.file.data.save(join(dirname(realpath(__file__)), "file_storage", filename))
+		absolute_path = safe_join(dirname(realpath(__file__)), "file_storage", filename)
 
-		#db.session.add(entry)
-		#db.session.commit()
+		form.file.data.save(absolute_path)
+		
+		entry = SpectraEntry()
+		
+		hdulist = fits.open(absolute_path)
+
+		entry.file = filename
+ 
+		entry.target_name = hdulist[0].header['OBJNAME']
+		entry.observator = hdulist[0].header['OBSERVER']
+
+		entry.ra = hdulist[0].header['RA']
+		entry.dec = hdulist[0].header['DEC']
+
+		entry.mjd = hdulist[0].header['JD-OBS']
+
+		entry.site = hdulist[0].header['BSS_SITE']
+
+		hdulist.close()
+
+		db.session.add(entry)
+		db.session.commit()
 
 		flash("Upload Successful!")
 
